ec7e8b4ae9ccd22e69212b46c8d2286f254a7a4b
Gabriel Rocha de Souza <souza.gabriel.0210@gmail.com>
2023-01-24 00:38:27 -0300

    URI 1661 and 1128(wp)

--- Atividades/Beecrowd/Python/1128.py
+++ Atividades/Beecrowd/Python/1128.py
@@ -1,35 +1,26 @@
 # Work in progress
 
-def Juntar(L1,L2):
-    for a in range(len(L2)):
-        if L2[a] not in L1:
-            L1.append(L2[a])
-    return L1
+def bfs(grafo, vertice_fonte):
+    visitados, fila = set(), [vertice_fonte]
+    while fila:
+        vertice = fila.pop(0)
+        if vertice not in visitados:
+            visitados.add(vertice)
+            fila.extend(set(grafo[vertice]) - set(visitados))
+    return visitados
 
 while True:
-    Graf_inter = {}
-    Graf_nodo = []
-    Inter,Rua = list(map(int,input().split(" ")))
-    if Inter == 0 and Rua == 0: break
-    for a in range(Rua):
+    N,M = list(map(int,input().split(" ")))
+    if (N == 0) and (M == 0): break
+    nodos = list([] for a in range (0, N))
+    for b in range(M):
         V,W,P = list(map(int,input().split(" ")))
-        if V not in Graf_nodo:
-            Graf_nodo.append(V)
-            Graf_inter[V] = [W]
-        else: Graf_inter[V].append(W)
-        if P == 2: 
-            if W not in Graf_nodo: 
-                Graf_nodo.append(W)
-                Graf_inter[W] = [V]
-            else: Graf_inter[W].append(V)
-
-    usados,index = [],0
-    ref = Juntar([],Graf_inter[1])
-    while True:
-        try:
-            if ref[index] in usados: break
-        except: break
-        usados.append(ref[index])
-        ref = Juntar(ref,Graf_inter[ref[index]])
-        index += 1
-    print(1 if len(ref) == Inter else 0)
+        nodos[V-1].append(W-1)
+        if P == 2: nodos[W-1].append(V-1)
+    
+    Teste = True
+    for c in range(N):
+        if len(bfs(nodos,c)) != N:
+            Teste = False
+            break
+    print(1 if Teste else 0)

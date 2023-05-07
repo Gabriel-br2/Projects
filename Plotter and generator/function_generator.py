9f2eb5ed2c519f4f072c2ba99556c53119f12d05
Gabriel Rocha de Souza <souza.gabriel.0210@gmail.com>
2023-05-07 20:16:43 -0300

    Delete Plotter and generator directory

--- Plotter and generator/function_generator.py
+++
@@ -1,8 +0,0 @@
-min_value, max_value = list(map(int,input("valor minimo e maximo: ").split(" ")))
-arq = open("dados.txt","w")
-X = min_value
-while X < max_value:
-    Y = #function to be ploted
-    arq.write(str(X)+","+str(Y)+"\n")
-    X += 1
-arq.close()
